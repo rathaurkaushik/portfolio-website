21c35bdc2db7e08cf197346fcf8bd171bc87b29c
rathaurkaushik <rathaurkaushik015@gmail.com>
2025-08-27 18:06:36 +0530

    all images line added

---
+++ lib/constant/app_image.dart
@@ -0,0 +1,78 @@
+class AppImages {
+  /// Profile
+  static const String my_image =
+'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/my_image%2FmyImage.png?alt=media&token=48423239-ccbf-46d4-ab68-f4180283b0ca';
+  static const String about_image =
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/my_image%2Fabout_image.jpg?alt=media&token=fab1b4bf-a297-4748-bc12-9385df498b92";
+
+  /// Skills
+  static const String airflow =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/skills%2Fairflow.png?alt=media&token=ec58c940-aa8b-4566-9f64-b1e305657c14';
+  static const String api =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/skills%2Fapi.png?alt=media&token=b73caa2f-5bca-4be9-9cb8-51d8eb0387c0';
+  static const String aws =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/skills%2Faws.png?alt=media&token=a105856c-4bb1-4fb8-b138-0a6795abe873';
+  static const String azure =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/skills%2Fazure.png?alt=media&token=6005675f-d555-4270-a4de-92ddd4c69696';
+  static const String bigquery =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/skills%2Fbigquery.png?alt=media&token=88607417-65c1-4154-a076-3ca9fd7b2bea';
+  static const String cplusplus =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/skills%2Fcplusplus.png?alt=media&token=db29ffc4-718a-460a-982b-ce07356baf72';
+  static const String dart =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/skills%2Fdart.png?alt=media&token=24fdf977-c36b-414a-93b4-8b8aed208dc9';
+  static const String databricks =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/skills%2Fdatabricks.png?alt=media&token=5de4f8ee-ce13-488b-a005-12fe2850fb97';
+  static const String docker =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/skills%2Fdocker.png?alt=media&token=5d150908-f0f1-4b38-9d8a-d825f7fe6477';
+  static const String figma =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/skills%2Ffigma.png?alt=media&token=1851f97f-1b27-4c09-be22-efcace799ea4';
+  static const String firebase =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/skills%2Ffirebase%20(2).png?alt=media&token=d60eebc4-7db3-4ff2-ae47-12c40a75762f';
+  static const String flutter =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/skills%2Fflutter.png?alt=media&token=9e8db4f5-e0cf-4feb-864a-d7d2915c7794';
+  static const String gcp =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/skills%2Fgcp.png?alt=media&token=42de59ca-b11f-4424-808b-b64939052619';
+  static const String github =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/skills%2Fgithub.png?alt=media&token=3e6814eb-9c75-4a0d-a85d-3a409ddff656';
+  static const String javascript =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/skills%2Fjavascript.png?alt=media&token=8f35c2b6-918c-440f-8408-32571af8de94';
+  static const String kafka =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/skills%2Fkafka.png?alt=media&token=ed93f492-ef1e-49cf-9cb0-1c8c31abb872';
+  static const String python =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/skills%2Fpython.png?alt=media&token=37d4d53a-8fbd-49b5-848c-fde246aa6d1b';
+  static const String snowflake =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/skills%2Fsnowflake.png?alt=media&token=23cd3380-74ed-45f8-bc62-4a4359686d37';
+  static const String spark =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/skills%2Fspark.png?alt=media&token=d9dd393d-1537-4edd-a88f-1df860aaf41d';
+  static const String sql =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/skills%2Fsql%20(3).png?alt=media&token=33845411-6cb0-459f-a2ad-84442d497e06';
+  static const String vscode =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/skills%2Fvscode.png?alt=media&token=05a2a8d1-0a93-4a02-b26d-dc4891c12df6';
+  static const String open_weather =
+  'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/skills%2Fopen_weather.png?alt=media&token=5f4e0cce-b7ee-4a2d-b438-b4cc08049513';
+
+  ///////
+  ///
+/// Experience
+  static const String data_engineer =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/experience%2Fdata_engineer.png?alt=media&token=e551a0a3-2086-4d85-a6fd-68d12081af9b';
+   static const String block_chain =
+   'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/experience%2Fblockchain.png?alt=media&token=a1e88447-0d95-4cc9-9363-3d385a215e30';
+  static const String backend =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/experience%2Fbackend.png?alt=media&token=386566b0-6cba-463e-9efb-85c5fd398b22';
+  static const String app =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/experience%2Fapp.png?alt=media&token=7f252f30-9c14-4452-bf07-b7583887b297';
+
+  ///
+  ///works
+  static const String weather =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/project%2Fweather_live.jpg?alt=media&token=239230fa-551a-48e1-81d0-402842a9c82d';
+  static const String shoes =
+'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/project%2Ffoot_wear_app.png?alt=media&token=27e239b6-05c2-422d-bead-86e4d8c0c4f3';
+  static const String task =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/project%2Ftask_manage_app.jpg?alt=media&token=601086e1-3f13-44a6-ab8a-20bba2a69cbb';
+  static const String portfolio =
+      'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/project%2Fportfolio_website.png?alt=media&token=9f02736d-1a8a-4b3f-b8a0-fda4e2ddbb68';
+  static const String smart =
+'https://firebasestorage.googleapis.com/v0/b/portfolio-a8ed3.firebasestorage.app/o/project%2Fsmart_clean.png?alt=media&token=fdaf066c-aa2e-4ebe-8406-9e8658f06ce5';
+}
